2276ef306d5522ed24cf0fcafab6fef4bd506a38
Robert Miller <romille@clarkson.edu>
2016-04-23 12:40:35 -0400

    added layouts and started options

--- app/src/main/java/app/shb/somershotbagels/Option.java
+++ app/src/main/java/app/shb/somershotbagels/Option.java
@@ -1,10 +1,13 @@
 package app.shb.somershotbagels;
 
 
+import android.widget.ToggleButton;
+
 /**
  * Created by hunter on 4/2/16.
  */
 public interface Option {
+
     @Override
     public String toString();
 }
