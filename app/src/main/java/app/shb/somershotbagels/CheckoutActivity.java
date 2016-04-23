f350fce696a7f25f9857585e6b2cd2c8a2b10250
Robert Miller <romille@clarkson.edu>
2016-04-22 20:42:36 -0400

    Finished the socket stuff
    changed the socket and added a toast after the order is sent

--- app/src/main/java/app/shb/somershotbagels/CheckoutActivity.java
+++ app/src/main/java/app/shb/somershotbagels/CheckoutActivity.java
@@ -13,6 +13,8 @@ import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -33,7 +35,7 @@ public class CheckoutActivity extends AppCompatActivity{
     private boolean nameEnter = false, phoneEnter = false, emailEnter = false;
     private Timer emailTimer = new Timer();
     private Timer phoneTimer = new Timer();
-    private final long DELAY = 750;
+    private final long DELAY = 1000;
     private ReentrantLock lock = new ReentrantLock();
     private EditText nameEdit;
     private EditText emailEdit;
@@ -191,19 +193,29 @@ public class CheckoutActivity extends AppCompatActivity{
                     public void run() {
                         Log.d("text", "CLICKED");
                         try {
-
-                            Socket socket = new Socket("localhost", 5969);
+                            InetAddress i = InetAddress.getByName("cisco09122.townhouse.clarkson.edu");
+                            Socket socket = new Socket(i, 5969);
                             Log.d("text", "ORDERING");
                             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                            BufferedReader in = new BufferedReader(
+                                    new InputStreamReader(socket.getInputStream()));
                             out.println("<order>");
                             out.println("Name: " + nameEdit.getText().toString());
                             out.println("Phone: " + phoneEdit.getText().toString());
                             out.println("Email: " + emailEdit.getText().toString());
-                            out.println(order.toString());
                             out.println("</order>");
+
+
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    Toast t = Toast.makeText(CheckoutActivity.this, "Order Received", Toast.LENGTH_LONG);
+                                    t.setGravity(Gravity.CENTER, 0, 0);
+                                    t.show();
+                                }
+                            });
                             out.close();
                             socket.close();
-                        } catch (Exception e) {
+                        }catch (Exception e) {
                             Log.d("text", e.toString());
                         }
                     }
