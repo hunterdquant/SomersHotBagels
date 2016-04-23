0c55dd8c18a246a6846ee6df535b32c2f0402daf
Hunter Quant <hunterdquant@gmail.com>
2016-04-23 11:35:02 -0400

    added fragment transaction for item edit fragment

--- app/src/main/java/app/shb/somershotbagels/ListItemsFragment.java
+++ app/src/main/java/app/shb/somershotbagels/ListItemsFragment.java
@@ -3,12 +3,14 @@ package app.shb.somershotbagels;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -24,6 +26,7 @@ import java.util.List;
     public class ListItemsFragment extends Fragment {
 
     OrderTransfer orderTransfer;
+    ListView listView;
     private Order order;
     private List<Item> itemList;
     private List<String> itemNames;
@@ -40,7 +43,23 @@ import java.util.List;
         itemList = MenuRetrieval.retreveCategory(MenuRetrieval.CATEGORY_NAMES[listIndex]);
         order = orderTransfer.getOrder();
 
-        ListView listView = (ListView) root.findViewById(R.id.itemListActivity);
+        listView = (ListView) root.findViewById(R.id.itemListActivity);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                Bundle bundle = new Bundle();
+                bundle.putInt("LIST_INDEX", position);
+
+                ItemEditFragment ief = new ItemEditFragment();
+                ief.setArguments(bundle);
+
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.addToBackStack(null);
+                ft.replace(R.id.homeContainer, ief);
+                ft.commit();
+            }
+        });
         itemNames = new ArrayList<String>();
         populateItemNames();
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
