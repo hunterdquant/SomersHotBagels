47c1775232d03d80350fc4e173fce843283d706a
Hunter Quant <hunterdquant@gmail.com>
2016-04-27 18:36:28 -0400

    changed popup in cart to an alert

--- app/src/main/java/app/shb/somershotbagels/CartFragment.java
+++ app/src/main/java/app/shb/somershotbagels/CartFragment.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.PopupMenu;
 import android.text.InputType;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,6 +18,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 import com.google.gson.Gson;
@@ -25,8 +27,8 @@ import java.util.List;
 
 /**
  * Handles the display and functionality of the Cart tab.
- * @author Hunter Quant, Robert Miller
  *
+ * @author Hunter Quant, Robert Miller
  */
 public class CartFragment extends Fragment {
 
@@ -70,25 +72,43 @@ public class CartFragment extends Fragment {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
-                //Creating the instance of PopupMenu
-                PopupMenu popup = new PopupMenu(getActivity(), listView);
-                //Inflating the Popup using xml file
-                popup.getMenuInflater().inflate(R.menu.cart_popup, popup.getMenu());
 
-                //registering popup with OnMenuItemClickListener
-                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    public boolean onMenuItemClick(MenuItem item) {
-                        // Remove tagged elements.
-                        if (item.getTitle().equals("Remove")) {
-                            order.removeItem(position);
-                            orderTransfer.updateCart();
-                            Toast.makeText(getActivity(), "Item removed from the cart.", Toast.LENGTH_SHORT).show();
-                        }
-                        return true;
+                final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setCancelable(true);
+                builder.setTitle("Would you like to remove " + order.getItems().get(position).getName());
+                //builder.setMessage(message);
+
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        order.removeItem(position);
+                        orderTransfer.updateCart();
+                        Toast.makeText(getActivity(), "Item removed from the cart.", Toast.LENGTH_SHORT).show();
                     }
                 });
 
-                popup.show();//showing popup menu
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+
+
+                final AlertDialog dialog = builder.create();
+                dialog.show(); //show() should be called before dialog.getButton().
+
+
+                final Button positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                LinearLayout.LayoutParams positiveButtonLL = (LinearLayout.LayoutParams) positiveButton.getLayoutParams();
+                positiveButtonLL.gravity = Gravity.CENTER;
+                positiveButton.setLayoutParams(positiveButtonLL);
+
+                final Button negativeButton = dialog.getButton(AlertDialog.BUTTON_NEGATIVE);
+                LinearLayout.LayoutParams negativeButtonLL = (LinearLayout.LayoutParams) negativeButton.getLayoutParams();
+                negativeButtonLL.gravity = Gravity.CENTER;
+                negativeButton.setLayoutParams(negativeButtonLL);
             }
         });
 
