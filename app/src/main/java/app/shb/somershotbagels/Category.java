b7cf3263818fb321cf7784026a9a46518bf4452c
Hunter Quant <hunterdquant@gmail.com>
2016-04-27 20:13:27 -0400

    documented category

--- app/src/main/java/app/shb/somershotbagels/Category.java
+++ app/src/main/java/app/shb/somershotbagels/Category.java
@@ -6,34 +6,50 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Created by hunter on 4/2/16.
+ * Represents the image and name of a category.
+ *
+ * @author Hunter Quant, Robert Miller
  */
 public class Category {
+
+    /*
+      The resource id of the image corresponding the category.
+     */
     private int imageId;
+    /*
+      The name of the category.
+     */
     private String name;
 
     public Category(int imageId, String name) {
         this.imageId = imageId;
         this.name = name;
-        buildMapping();
     }
 
-    private void buildMapping() {
-
-    }
-
+    /**
+     * @return the image id.
+     */
     public int getImageId() {
         return imageId;
     }
 
+    /**
+     * @param imageId the new image id.
+     */
     public void setImageId(int imageId) {
         this.imageId = imageId;
     }
 
+    /**
+     * @return the name of the category.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @param name the new category name.
+     */
     public void setName(String name) {
         this.name = name;
     }
