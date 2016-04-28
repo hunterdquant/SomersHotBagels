59abc0357200153be86b6d0a8df927c0ce437df2
Hunter Quant <hunterdquant@gmail.com>
2016-04-27 21:05:46 -0400

    documented HomeFragment

--- app/src/main/java/app/shb/somershotbagels/HomeFragment.java
+++ app/src/main/java/app/shb/somershotbagels/HomeFragment.java
@@ -1,40 +1,22 @@
 package app.shb.somershotbagels;
 
-
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.widget.PopupMenu;
-import android.text.InputType;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import com.google.gson.Gson;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
- * Created by hunter on 3/25/16.
+ * This fragment represents a container for fragment transactions.
+ *
+ * @author Hunter Quant, Robert Miller
  */
 public class HomeFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Replace initially withe the CategoryListFragment.
         View root = inflater.inflate(R.layout.home_tab_fragment, container, false);
         CategoryListFragment clf = new CategoryListFragment();
         FragmentTransaction ft = getFragmentManager().beginTransaction();
