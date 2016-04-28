21aed0e6e61aa1a656ed519dcc3731c1289cef8f
Hunter Quant <hunterdquant@gmail.com>
2016-04-27 22:07:22 -0400

    documented Splash

--- app/src/main/java/app/shb/somershotbagels/Splash.java
+++ app/src/main/java/app/shb/somershotbagels/Splash.java
@@ -5,22 +5,26 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
+/**
+ * Displays the splash screen for SPLASH_TIME seconds.
+ *
+ * @author Hunter Quant, Robert Miller
+ */
 public class Splash extends AppCompatActivity {
 
+    /*
+      The time to display.
+     */
     private final int SPLASH_TIME = 2000;
 
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView(R.layout.splash);
 
-        /* New Handler to start the Menu-Activity 
-         * and close this Splash-Screen after some seconds.*/
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
-                /* Create an Intent that will start the Menu-Activity. */
                 Intent mainIntent = new Intent(Splash.this,SHBActivity.class);
                 Splash.this.startActivity(mainIntent);
                 Splash.this.finish();
