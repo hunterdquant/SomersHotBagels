b1e84060a705f65e5462dc81b15d5c8b95a33d11
Hunter Quant <hunterdquant@gmail.com>
2016-04-27 20:28:17 -0400

    documented CategoryListFragment

--- app/src/main/java/app/shb/somershotbagels/Category.java
+++ app/src/main/java/app/shb/somershotbagels/Category.java
@@ -1,10 +1,5 @@
 package app.shb.somershotbagels;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 /**
  * Represents the image and name of a category.
  *
--- app/src/main/java/app/shb/somershotbagels/CategoryListFragment.java
+++ app/src/main/java/app/shb/somershotbagels/CategoryListFragment.java
@@ -1,39 +1,41 @@
 package app.shb.somershotbagels;
 
 import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.widget.PopupMenu;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
-
-import com.google.gson.Gson;
-
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+
 /**
- * Created by hunter on 3/25/16.
+ * Handles the display and functionality of the category list fragment.
+ *
+ * @author Hunter Quant, Robert Miller
  */
 public class CategoryListFragment extends Fragment {
 
+    /*
+      Callback to communicate with the activity.
+     */
     OrderTransfer orderTransfer;
+    /*
+      Cart list view.
+     */
     ListView listView;
+    /*
+      A list of category names.
+     */
     List<Category> categoryList;
+    /*
+      Reference to the activity's global order.
+     */
     private Order order;
-
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -44,6 +46,8 @@ public class CategoryListFragment extends Fragment {
             categoryList.add(category);
         }
         listView = (ListView) root.findViewById(R.id.categoryList);
+
+        // Do a fragment transaction with the selected item.
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
@@ -65,6 +69,11 @@ public class CategoryListFragment extends Fragment {
         return root;
     }
 
+    /**
+     * Sets the order on activity attachment
+     *
+     * @param context
+     */
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
